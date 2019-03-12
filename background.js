24f4243bf5dfb83252bfeb6b5aa80620a2808f71
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2019-03-11 18:33:08 -0600

    remove whitespace

--- background.js
+++ background.js
@@ -1,12 +1,10 @@
-console.log("bg.js");
+console.log("background.js loaded...");
 
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(){
-
-  chrome.tabs.query({'active': true, 'currentWindow': true}, function (tabs) {
-    var url = tabs;
+  chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
+    var url = tab[0].url;
     console.log(url);
   });
-
 }
