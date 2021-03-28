ed868b0a047b9a0dd19bf9d3fe44d747379ad109
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2021-03-27 19:18:48 -0600

    Make post to server

--- background.js
+++ background.js
@@ -3,8 +3,27 @@ console.log("background.js loaded...");
 chrome.browserAction.onClicked.addListener(buttonClicked);
 
 function buttonClicked(){
-  chrome.tabs.query({'active': true, 'currentWindow': true}, function (tab) {
-    var url = tab[0].url;
-    console.log(url);
-  });
+  chrome.tabs.query({
+    active: true,
+    lastFocusedWindow: true
+  }, function (tabs) {
+    // and use that tab to fill in out title and url
+    var tab = tabs[0];
+
+    let data = {"title": tab.title, "url": tab.url}
+    fetch('http://localhost:5000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  })
 }
